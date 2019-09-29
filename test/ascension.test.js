626dec46cfddc6d3b99502a35d50c7e93ca515ca
Alan Gutierrez <alan@prettyrobots.com>
2019-09-29 10:53:32 -0500

    Revert tests to Proof.
    See #22.

--- test/ascension.test.js
+++ test/ascension.test.js
@@ -1,58 +1,52 @@
-describe('ascension', () => {
+require('proof')(9, (okay) => {
     const ascension = require('..')
-    const assert = require('assert')
-    it('can create a comparator from a function', () => {
-        const comparator = ascension([ function (left, right) {
-            return left - right
-        } ], function (object) { return [ object ] })
 
-        assert(comparator(1, 2) < 0, 'less than')
-        assert(comparator(2, 1) > 0, 'greater than')
-        assert(comparator(1, 1) == 0, 'equal')
+    const comparator = ascension([ function (left, right) {
+        return left - right
+    } ], function (object) { return [ object ] })
+
+    okay(comparator(1, 2) < 0, 'less than')
+    okay(comparator(2, 1) > 0, 'greater than')
+    okay(comparator(1, 1) == 0, 'equal')
+
+    const composite = ascension([ Number, String ], function (object) {
+        return [ object.number, object.string ]
     })
 
-    it('can create a comparator from a type', () => {
-        const comparator = ascension([ Number, String ], function (object) {
-            return [ object.number, object.string ]
-        })
+    okay(composite({
+        number: 1, string: 'a'
+    }, {
+        number: 1, string: 'b'
+    }) < 0, 'composite key second column less then')
 
-        assert(comparator({
-            number: 1, string: 'a'
-        }, {
-            number: 1, string: 'b'
-        }) < 0, 'composite key second column less then')
+    okay(composite({
+        number: 1, string: 'b'
+    }, {
+        number: 1, string: 'a'
+    }) > 0, 'string greater than')
 
-        assert(comparator({
-            number: 1, string: 'b'
-        }, {
-            number: 1, string: 'a'
-        }) > 0, 'string greater than')
+    okay(composite({
+        number: 1, string: 'a'
+    }, {
+        number: 1, string: 'a'
+    }) == 0, 'composite and string equal')
 
-        assert(comparator({
-            number: 1, string: 'a'
-        }, {
-            number: 1, string: 'a'
-        }) == 0, 'composite and string equal')
+    const bigints = ascension([ BigInt ], function (object) {
+        return [ object.integer ]
     })
-
-    it('can create a bigints', () => {
-        const comparator = ascension([ BigInt ], function (object) {
-            return [ object.integer ]
-        })
-        assert(comparator({
-            integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
-        }, {
-            integer: 0n
-        }) > 0, 'greater than')
-        assert(comparator({
-            integer: 0n
-        }, {
-            integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
-        }) < 0, 'less than')
-        assert(comparator({
-            integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
-        }, {
-            integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
-        }) == 0, 'equal')
-    })
+    okay(bigints({
+        integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
+    }, {
+        integer: 0n
+    }) > 0, 'bigint greater than')
+    okay(bigints({
+        integer: 0n
+    }, {
+        integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
+    }) < 0, 'bigint less than')
+    okay(bigints({
+        integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
+    }, {
+        integer: BigInt(Number.MAX_SAFE_INTEGER) * 2n
+    }) == 0, 'bigint equal')
 })
